4b4689d98e6d2cec1a6c8a6773a1daab83462f2c
ravindrarana <projectsaathi@gmail.com>
2023-01-21 16:18:14 +0545

    Web Technology II

---
+++ Unit_1/hello_world/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello World</title>
+</head>
+<body>
+    <!-- PHP Code Start -->
+    <?php
+        echo "Hello World";
+    ?>
+    <!-- End Code -->
+    <!-- display in H1 Tag  -->
+    <h1><?php  echo "Hello World !"; ?></h1>
+</body>
+</html>
