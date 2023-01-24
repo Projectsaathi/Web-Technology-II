5d5541cdd1a98a8d5104152b5075be815f67ec32
ravindrarana <projectsaathi@gmail.com>
2023-01-24 09:03:02 +0545

    empty string updated

--- Unit_6/empty_string/index.php
+++ Unit_6/empty_string/index.php
@@ -5,9 +5,20 @@
     <title>Empty String Validation</title>
 </head>
 <body>
-    <form action="#">
+    <form action="#" method="POST">
         <input type="text" name="stringName" placeholder="Enter Any String">
         <input type="submit">
     </form>
+
+    <?php
+        $string = $_POST['stringName'];
+        if(empty($string))
+        {
+            echo "Please, enter any string";
+        }
+        else{
+            echo "Your entered String is:  " . $string;
+        }
+    ?>
 </body>
 </html>
