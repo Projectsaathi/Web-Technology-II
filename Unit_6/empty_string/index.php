06fde6c20c0c9c1645ec5530f6426f7366cb8c89
ravindrarana <projectsaathi@gmail.com>
2023-01-24 08:55:28 +0545

    Empty String Validation

---
+++ Unit_6/empty_string/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Empty String Validation</title>
+</head>
+<body>
+    <form action="#">
+        <input type="text" name="stringName" placeholder="Enter Any String">
+        <input type="submit">
+    </form>
+</body>
+</html>
